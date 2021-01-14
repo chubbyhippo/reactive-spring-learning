831f06444b5ccc79dfe54dde5f8cf7db18d7980e
ch <mkmsacc@outlook.com>
2021-01-14 16:48:16 +0700

    Add dev only customer service

---
+++ chapter03/bootstrap/src/main/java/com/example/bootstrap/hardcoded/DevelopmentOnlyCustomerService.java
@@ -0,0 +1,23 @@
+package com.example.bootstrap.hardcoded;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import com.example.bootstrap.BaseCustomerService;
+import com.example.bootstrap.DataSourceUtils;
+
+public class DevelopmentOnlyCustomerService extends BaseCustomerService {
+
+	protected DevelopmentOnlyCustomerService(DataSource ds) {
+		super(ds);
+	}
+
+	private static DataSource buildDataSource() {
+		DataSource dataSource = new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2).build();
+		return DataSourceUtils.initializedDbl(dataSource);
+	}
+
+}
