221dfd5d91855c0d8b80841303ad35add347ee98
ch <mkmsacc@outlook.com>
2021-01-17 21:44:40 +0700

    Disable test

--- chapter03/bootstrap/src/test/java/com/example/bootstrap/BootstrapApplicationTests.java
+++ chapter03/bootstrap/src/test/java/com/example/bootstrap/BootstrapApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.bootstrap;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BootstrapApplicationTests {
 
-	@Test
+	@Disabled
 	void contextLoads() {
 	}
 }
