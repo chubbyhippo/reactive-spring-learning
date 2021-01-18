6271f53fa8b44d9ddadd898ebebca3435078bd0d
ch <mkmsacc@outlook.com>
2021-01-18 14:30:08 +0700

    Add Synchronous

---
+++ chapter04/io/src/main/java/com/example/io/Synchronous.java
@@ -0,0 +1,30 @@
+package com.example.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.function.Consumer;
+
+import org.springframework.util.FileCopyUtils;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class Synchronous implements Reader {
+	
+	@Override
+	public void read(File file, Consumer<Bytes> consumer, Runnable finished)
+			throws IOException {
+
+		try (FileInputStream in = new FileInputStream(file)) {
+			byte[] data = new byte[FileCopyUtils.BUFFER_SIZE];
+			int res;
+			while ((res = in.read(data, 0, data.length)) != -1) {
+				consumer.accept(Bytes.from(data, res));
+			}
+			finished.run();
+		}
+
+	}
+
+}
