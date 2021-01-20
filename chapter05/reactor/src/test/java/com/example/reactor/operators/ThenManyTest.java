9db25fe08d6cbc254bcc2f006aa22d9617a1671c
ch <mkmsacc@outlook.com>
2021-01-20 13:05:16 +0700

    Add ThenManyTest

---
+++ chapter05/reactor/src/test/java/com/example/reactor/operators/ThenManyTest.java
@@ -0,0 +1,33 @@
+package com.example.reactor.operators;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class ThenManyTest {
+
+	@Test
+	public void thenMany() {
+		var letters = new AtomicInteger();
+		var numbers = new AtomicInteger();
+
+		Flux<String> lettersPublisher = Flux.just("a", "b", "c")
+				.doOnNext(value -> letters.incrementAndGet());
+		Flux<Integer> numbersPublisher = Flux.just(1, 2, 3)
+				.doOnNext(number -> numbers.incrementAndGet());
+		Flux<Integer> thisBeforeThat = lettersPublisher
+				.thenMany(numbersPublisher);
+		StepVerifier.create(thisBeforeThat).expectNext(1, 2, 3)
+				.verifyComplete();
+
+		assertThat(letters.get()).isEqualTo(3);
+		assertThat(numbers.get()).isEqualTo(3);
+
+	}
+
+}
