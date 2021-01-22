4449a8c2794e544319fe807d71d695f678993e49
ch <mkmsacc@outlook.com>
2021-01-22 19:28:03 +0700

    Organized imports

--- chapter05/reactor/src/test/java/com/example/reactor/hotandcoldstreams/HotStreamTest1.java
+++ chapter05/reactor/src/test/java/com/example/reactor/hotandcoldstreams/HotStreamTest1.java
@@ -4,16 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 import java.util.function.Consumer;
 
 import org.junit.jupiter.api.Test;
 
-import reactor.core.publisher.EmitterProcessor;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Sinks;
-import reactor.test.StepVerifier;
 
 public class HotStreamTest1 {
 
@@ -25,15 +20,16 @@ public class HotStreamTest1 {
 	public void hot() throws Exception {
 		var first = new ArrayList<Integer>();
 		var second = new ArrayList<Integer>();
-		
-		Sinks.Many<Integer> sinks = Sinks.many().multicast().onBackpressureBuffer(2);
+
+		Sinks.Many<Integer> sinks = Sinks.many().multicast()
+				.onBackpressureBuffer(2);
 		sinks.tryEmitNext(1);
 		sinks.tryEmitNext(2);
 		sinks.asFlux().subscribe(collect(first));
 		sinks.tryEmitNext(3);
 		sinks.asFlux().subscribe(collect(second));
 		sinks.tryEmitComplete();
-		
+
 		assertThat(first.size() > second.size()).isTrue();
 
 	}
