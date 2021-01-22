c77664c3e1c9ba0007a7010f42969fea7554d97e
ch <mkmsacc@outlook.com>
2021-01-22 16:14:34 +0700

    Add ControlFlowZipTest

---
+++ chapter05/reactor/src/test/java/com/example/reactor/controlflow/ControlFlowZipTest.java
@@ -0,0 +1,24 @@
+package com.example.reactor.controlflow;
+
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class ControlFlowZipTest {
+	
+	private String from(Integer i, String s) {
+		return i + ":" + s;
+	}
+	
+	@Test
+	public void zip() {
+		Flux<Integer> first = Flux.just(1, 2, 3);
+		Flux<String> second = Flux.just("a", "b", "c");
+		Flux<String> zip = Flux.zip(first, second).map(tuple -> from(tuple.getT1(), tuple.getT2()));
+		
+		StepVerifier.create(zip).expectNext("1:a", "2:b", "3:c").verifyComplete();
+		
+	}
+
+}
