7e121774d9c8f883086d62835f08a575b9ab00a0
ch <mkmsacc@outlook.com>
2021-01-24 10:20:32 +0700

    Add CustomerTest

---
+++ chapter06/r2dbc/src/main/java/com/example/r2dbc/CustomerService.java
@@ -0,0 +1,49 @@
+package com.example.r2dbc;
+
+import org.reactivestreams.Publisher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.reactive.TransactionalOperator;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+	private final SimpleCustomerRepository repository;
+
+	private final TransactionalOperator operator;
+
+	private final CustomerDatabaseInitializer initializer;
+
+	public Publisher<Void> resetDatabase() {
+		return this.initializer.resetCustomerTable();
+	}
+
+	public Flux<Customer> upsert(String email) {
+		var customers = this.repository.findAll()
+				.filter(customer -> customer.getEmail().equalsIgnoreCase(email))
+				.flatMap(match -> this.repository
+						.update(new Customer(match.getId(), email)))
+				.switchIfEmpty(this.repository.save(new Customer(null, email)));
+		var validatedResults = errorIfEmailsAreInvalid(customers);
+		return this.operator.transactional(validatedResults);
+	}
+
+	@Transactional
+	public Flux<Customer> normalizeEmails() {
+		return errorIfEmailsAreInvalid(this.repository.findAll()
+				.flatMap(x -> this.upsert(x.getEmail().toUpperCase())));
+	}
+
+	private static Flux<Customer> errorIfEmailsAreInvalid(
+			Flux<Customer> input) {
+		return input.filter(c -> c.getEmail().contains("@"))
+				.switchIfEmpty(Mono.error(new IllegalArgumentException(
+						"the email needs to be of the form a@b.com!")));
+	}
+
+}
