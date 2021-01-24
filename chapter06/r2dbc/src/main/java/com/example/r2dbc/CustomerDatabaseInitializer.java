d531527c0ed8b99b8f89b2795993c04466c6bb96
ch <mkmsacc@outlook.com>
2021-01-24 10:11:58 +0700

    Add CustomerDatabaseinitializer

---
+++ chapter06/r2dbc/src/main/java/com/example/r2dbc/CustomerDatabaseInitializer.java
@@ -0,0 +1,52 @@
+package com.example.r2dbc;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.reactive.TransactionalOperator;
+import org.springframework.util.FileCopyUtils;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+class CustomerDatabaseInitializer {
+
+	private final TransactionalOperator transactionalOperator;
+
+	private final DatabaseClient client;
+
+	private final String sql;
+
+	private final SimpleCustomerRepository repository;
+	
+	CustomerDatabaseInitializer(@Value("classpath:/schema.sql") Resource resource,
+			DatabaseClient client, SimpleCustomerRepository repository,
+			TransactionalOperator operator) throws Exception {
+
+		this.client = client;
+		this.repository = repository;
+		this.transactionalOperator = operator;
+
+		try (Reader in = new InputStreamReader(resource.getInputStream())) {
+			this.sql = FileCopyUtils.copyToString(in);
+		}
+	}
+
+	public Publisher<Void> resetCustomerTable() {
+	
+		Mono<Void> createSchema = client.sql(sql).then()
+				.onErrorResume(throwable -> Mono.empty());
+		Flux<Void> findAndDelete = repository.findAll()
+				.flatMap(customer -> repository.deleteById(customer.getId()));
+
+		return createSchema
+				.thenMany(this.transactionalOperator.execute(status -> findAndDelete));
+	}
+
+}
