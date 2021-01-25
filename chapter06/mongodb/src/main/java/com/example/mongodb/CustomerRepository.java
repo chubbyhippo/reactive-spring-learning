98d9f6ca2861bb4b65f88c064ca97e33190eeb6f
ch <mkmsacc@outlook.com>
2021-01-25 09:10:51 +0700

    Add CustomerRepository

---
+++ chapter06/mongodb/src/main/java/com/example/mongodb/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.example.mongodb;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+
+import reactor.core.publisher.Flux;
+
+public interface CustomerRepository extends ReactiveMongoRepository<Customer, String> {
+	
+	@Tailable
+	Flux<Customer> findByName(String name);
+
+}
