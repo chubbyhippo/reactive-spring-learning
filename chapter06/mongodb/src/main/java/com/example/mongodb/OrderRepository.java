d87e2c02c725400d42ad0a2060f3e6a2bcf24cd4
ch <mkmsacc@outlook.com>
2021-01-25 09:11:11 +0700

    Add OrderRepository

---
+++ chapter06/mongodb/src/main/java/com/example/mongodb/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.example.mongodb;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import reactor.core.publisher.Flux;
+
+public interface OrderRepository extends ReactiveCrudRepository<Order, String>{
+	
+	Flux<Order> findByProductId(String productId);
+
+}
