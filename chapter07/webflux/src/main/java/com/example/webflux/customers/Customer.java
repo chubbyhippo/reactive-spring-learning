c7079ef8496914ebe47943beab70e21e02edd5f5
ch <mkmsacc@outlook.com>
2021-01-26 11:32:54 +0700

    Add Customer

---
+++ chapter07/webflux/src/main/java/com/example/webflux/customers/Customer.java
@@ -0,0 +1,14 @@
+package com.example.webflux.customers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+	
+	private String id, name;
+
+}
