c199a3ed3c16cfe6838156fc84df684a053a8d20
ch <mkmsacc@outlook.com>
2021-01-27 17:21:27 +0700

    Add CaseInsensitiveRequestPredicate

---
+++ chapter07/webflux/src/main/java/com/example/webflux/routes/CaseInsensitiveRequestPredicate.java
@@ -0,0 +1,34 @@
+package com.example.webflux.routes;
+
+import org.springframework.web.reactive.function.server.RequestPredicate;
+import org.springframework.web.reactive.function.server.ServerRequest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RequiredArgsConstructor
+@Log4j2
+public class CaseInsensitiveRequestPredicate implements RequestPredicate {
+
+	private final RequestPredicate target;
+
+	public static RequestPredicate i(RequestPredicate rp) {
+		return new CaseInsensitiveRequestPredicate(rp);
+	}
+
+	@Override
+	public boolean test(ServerRequest request) {
+		log.info("test was called");
+		log.info("test content was " + new LowercaseUriServerRequestWrapper(request).uri());
+		log.info("test result was " + target.test(new LowercaseUriServerRequestWrapper(request)));
+//		return target.test(new LowercaseUriServerRequestWrapper(request).uri());
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		log.info("target = " + target.toString());
+		return target.toString();
+	}
+
+}
