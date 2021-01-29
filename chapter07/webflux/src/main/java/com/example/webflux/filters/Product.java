6b2f53d8bd85ad91e0fd496e18816bde2f4eff25
ch <mkmsacc@outlook.com>
2021-01-29 08:22:35 +0700

    Add Product

---
+++ chapter07/webflux/src/main/java/com/example/webflux/filters/Product.java
@@ -0,0 +1,12 @@
+package com.example.webflux.filters;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Product {
+	
+	private final String id;
+
+}
