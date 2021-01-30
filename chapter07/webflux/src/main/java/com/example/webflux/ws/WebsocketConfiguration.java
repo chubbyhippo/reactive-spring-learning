a0ffc7c1b77a02975d90674e5a80eba8ff040bc1
ch <mkmsacc@outlook.com>
2021-01-30 10:29:55 +0700

    Add WebsocketConfiguration

---
+++ chapter07/webflux/src/main/java/com/example/webflux/ws/WebsocketConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.webflux.ws;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+
+@Configuration
+public class WebsocketConfiguration {
+	
+	@Bean
+	WebSocketHandlerAdapter webSocketHandlerAdapter() {
+		return new WebSocketHandlerAdapter();
+	}
+
+}
