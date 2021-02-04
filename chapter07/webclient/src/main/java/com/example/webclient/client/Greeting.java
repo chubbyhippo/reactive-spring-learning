19341693d9f37dca5fad95f00dfa20b70ed4c322
ch <mkmsacc@outlook.com>
2021-02-04 10:17:01 +0700

    Add Greeting

---
+++ chapter07/webclient/src/main/java/com/example/webclient/client/Greeting.java
@@ -0,0 +1,16 @@
+package com.example.webclient.client;
+
+import lombok.NoArgsConstructor;
+
+import lombok.AllArgsConstructor;
+
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+	
+	private String message;
+
+}
